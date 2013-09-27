5597fb08452fc1d858ba1aaa05f185aa22b27410
Jeff Garretson <jeffgarretson@live.com>
2013-09-27 14:16:24 -0700

    Added experimental AD API

---
+++ FSIX.Web/Controllers/ADController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.DirectoryServices;
+using System.DirectoryServices.AccountManagement;
+using FSIX.Web.Models;
+
+namespace FSIX.Web.Controllers
+{
+    [Authorize]
+    public class ADController : ApiController
+    {
+        private readonly PrincipalContext _context;
+        public ADController()
+        {
+            _context = new PrincipalContext(ContextType.Domain);
+        }
+
+        // GET api/myadinfo
+        public ADUser GetMyInfo()
+        {
+            UserPrincipal user = UserPrincipal.FindByIdentity(_context, User.Identity.Name);
+            return new ADUser
+            {
+                Username = user.SamAccountName,
+                DisplayName = String.Format("{0} {1}", user.GivenName, user.Surname),
+                Email = user.EmailAddress
+            };
+        }
+
+        //public ICollection<ADUser> GetADUsers(string Term)
+        //{
+        //    ICollection<ADUser> users = new DirectorySearcher(Term).FindAll();
+        //    return users;
+        //}
+    }
+}
---
+++ FSIX.Web/Models/ADUser.cs
@@ -0,0 +1,9 @@
+﻿namespace FSIX.Web.Models
+{
+    public class ADUser
+    {
+        public string Username { get; set; }
+        public string DisplayName { get; set; }
+        public string Email { get; set; }
+    }
+}
